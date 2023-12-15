284e613584dfce184e758d34e85e6e5d2e80b007
Zero_Nodir <140510005+nodirqutfiddinov@users.noreply.github.com>
2023-12-15 13:50:01 +0300

    ыячва
    ачв

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="nodir">
+        <img src="" alt="" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
